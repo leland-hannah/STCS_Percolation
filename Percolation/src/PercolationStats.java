8d662f3505052ce9e30d07030798d72b898c355f
s-lelandh <s-lelandh@51S50021151.it.bsd405.org>
2017-09-24 18:57:38 -0700

    almost done with stats

--- Percolation/src/PercolationStats.java
+++ Percolation/src/PercolationStats.java
@@ -1,40 +1,56 @@
+import java.lang.reflect.Array;
+
 public class PercolationStats
 {
 	private int times;
 	private Percolation per;
-	private int n;
+	private double[] array;
 	private double openSites = 0;
     public PercolationStats(int N, int T)
     {
-    	n = N;
-        per = new Percolation(N);
+    	
+    	array = new double[T];
         for(int times = 0; times < T; times++)
         {
-        	int i = (int)(Math.random()*n+1);
-        	int j = (int)(Math.random()*n+1);
-        	per.open(i, j);
-        	openSites ++;
+        	per = new Percolation(N);
+        	
+        	while (!per.percolates())
+        	{
+        		int i = StdRandom.uniform(N);
+        		int j = StdRandom.uniform(N);
+        		per.open(i, j);
+        		openSites ++;
+        	}
+        	
+        	array[times] = (double) openSites/ (double) (N*N);
         }
     }
     
     public double mean()
     {
-        return openSites/times;
+        return StdStats.mean(array);
     }
     
     public double stddev()
     {
-        double top = Math.pow((openSites-mean()), 2);
-        return top/(times -1);
+    	return StdStats.stddev(array);
     }
     
     public double confidenceLo()
     {
-       return mean() - ((1.96*Math.sqrt(stddev()))/(Math.sqrt(times)));
+       return mean() - ((1.96*stddev())/(Math.sqrt(times)));
     }
     
     public double confidenceHi()
     {
-    	 return mean() + ((1.96*Math.sqrt(stddev()))/(Math.sqrt(times)));
+    	 return mean() + ((1.96*stddev())/(Math.sqrt(times)));
     }
+    
+    public static void main(String[] args)
+    {
+    	PercolationStats perlocation = new PercolationStats(20, 15);
+    	
+    }
+    
+    
 }
